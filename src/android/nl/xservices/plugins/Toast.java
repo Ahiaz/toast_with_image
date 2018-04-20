31d5ccbdd6ef5a2cf2af82b008a868363df98c06
Roy <corellasanchez@gmail.com>
2018-04-20 09:12:25 -0600

    fixes

--- src/android/nl/xservices/plugins/Toast.java
+++ src/android/nl/xservices/plugins/Toast.java
@@ -333,7 +333,6 @@ public class Toast extends CordovaPlugin {
 
 
                 //imageView.loadDataWithBaseURL(null, img, "html/css", "utf-8", null);
-                Picasso.get().load(url).into(imageView);
 
 
                 //imageView.setImageResource(cordova.getActivity().getResources().getIdentifier(url, "drawable", cordova.getActivity().getPackageName()));
@@ -348,6 +347,7 @@ public class Toast extends CordovaPlugin {
                 imageView.getLayoutParams().height = width;
                 imageView.getLayoutParams().width = width;
                 imageView.setScaleType(ImageView.ScaleType.FIT_XY);//important to get a square image with same dimensions
+                Picasso.get().load(url).into(imageView);
 
                 android.widget.Toast toastImage = new android.widget.Toast(contextToast);
 
